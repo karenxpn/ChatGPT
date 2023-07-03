976da4f58c83fca9afde2b926cdd8c3534156139
Karen Mirakyan <karenmirakyan@gmail.com>
2023-07-03 17:23:59 +0400

    first commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Karen Mirakyan on 03.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatGPT
+//
+//  Created by Karen Mirakyan on 03.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
